dd900959dd16a07e14335a5a62f5b0d2d76b3f16
sillah2010 <macktastic2014@gmail.com>
2017-10-13 13:40:41 -0400

    prepping heroku deployment

--- server.js
+++ server.js
@@ -20,9 +20,13 @@ connection.on('error', (err) => {
 })
 
 //Middleware
+app.use(express.static(`${__dirname}/client/build`))
 app.use(bodyParser.json())
 
-
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+  })
+  
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('App listening on port: ', PORT)
